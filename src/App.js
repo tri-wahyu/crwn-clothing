4ef44b6bf26e849063742ce264cd37aff442019f
Cupcake_Most <wahyu.teri@yahoo.com>
2022-01-03 08:10:11 +0700

    lesson 89

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import ShopePage from './pages/shop/shop.component';
 import Header from "./components/header/header.component";
-
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 function App() {
   return (
     <div>
@@ -14,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopePage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );

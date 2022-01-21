a65666f5bcf658499777926c059b986c6c18949b
Cupcake_Most <wahyu.teri@yahoo.com>
2022-01-21 18:23:38 +0700

    collection component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { Route } from "react-router-dom";
 
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import CollectionPage from "../../components/collection/collection.component";
 
-const ShopePage = () =>(
-    <div className="shop-page">
-        <CollectionsOverview />
-    </div>
-);
+
+const ShopePage = ({ match }) =>{
+    return (
+        <div className="shop-page">
+            <Route exact path={`${match.path}`} component={CollectionsOverview} />
+            <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+        </div>
+    );
+;}
 
 export default ShopePage;

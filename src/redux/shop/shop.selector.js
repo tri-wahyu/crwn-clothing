c881e32c90104e18baff454bc85167f10d28f8ff
Cupcake_Most <wahyu.teri@yahoo.com>
2022-01-21 18:35:47 +0700

    normalization

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -16,9 +16,14 @@ export const selectCollections = createSelector(
     shop => shop.collections
 )
 
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+);
+
 export const selectCollection = memoize((collectionUrlParam) => 
     createSelector(
         [selectCollections],
-        collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+        collections => collections[collectionUrlParam]
     )
 );
